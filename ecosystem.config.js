e521717a2b0a15a90132773c91d38eb1c8996ad8
Tushar Rajpara <tusharrajpara00@gmail.com>
2026-04-30 10:25:08 +0530

    cicd pipeline init

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "flow-stock-backend",
+      script: "./dist/index.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M",
+      env_file: "/home/deploy/flow_imitation_backend/.env",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+};
